0f526008969f6c5f637be43d731b1566029486a6
Guillaume <anonyme@example.com>
2024-12-04 11:47:23 +0100

    format doc + import React

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-// Ce fichier contient le composant principal de votre application. Vous pouvez également y importer les styles spécifiques à ce composant à l'aide de app.css.
+// Le fichier App.jsx définit la structure de la page et contient les composants principaux de ton application. Tu peux également y importer des styles spécifiques en utilisant un fichier comme App.css.
 
-import React from 'react';
-import './App.css'
+import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container } from "@mui/material";
+import Button from "@mui/material/Button";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import UserProfil from "./pages/UserProfil";
+import Wall from "./pages/Wall";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
+    <div className="App">
+      <h1>Bonjour React !</h1>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-// Ce fichier sert à monter l'application React sur l'élément root de votre fichier HTML. Il est généralement situé dans le dossier src.
+// Le fichier main.jsx a pour fonction principale de monter l'application React sur l'élément avec l'ID "root" dans ton fichier HTML. En le liant ainsi, tu permets à ton application React de s'intégrer dans la structure du DOM. Cela assure que tout le contenu et les composants définis dans App.jsx seront rendus à l'intérieur de cet élément HTML spécifique.
 
-// import React from 'react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './main.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App.jsx";
+import "./main.css";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App/>
-  </StrictMode>,
-)
-
-
+    <App />
+  </StrictMode>
+);
